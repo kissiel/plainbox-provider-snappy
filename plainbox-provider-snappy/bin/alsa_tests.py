28fbc56e38eadfd3d1064b56f2c73350a49c4e74
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2016-12-13 10:35:13 +0100

    add recording to alsa_tests and a loopback scenario

--- plainbox-provider-snappy/bin/alsa_tests.py
+++ plainbox-provider-snappy/bin/alsa_tests.py
@@ -9,7 +9,10 @@
 import alsaaudio
 import argparse
 import math
+import numpy
 import struct
+import sys
+import threading
 
 RATE = 44100
 PERIOD = 441  # chunk size (in samples) that will be used when talking to alsa
@@ -47,21 +50,63 @@ class Player:
         self.pcm.write(buff)
 
 
+class Recorder:
+    def __init__(self):
+        self.pcm = alsaaudio.PCM(alsaaudio.PCM_CAPTURE)
+        self.pcm.setchannels(1)
+        self.pcm.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
+        self.pcm.setperiodsize(PERIOD)
+
+    def record(self, length):
+        samples = []
+        while len(samples) < length:
+            raw = self.pcm.read()
+            samples += [x[0] for x in struct.iter_unpack("<f", raw[1])]
+
+        return samples
+
+
 def playback_test(seconds):
     player = Player()
     for chunk in sine(440, seconds * RATE, PERIOD):
         player.play(chunk)
 
 
+def loopback_test(seconds, freq = 455.5):
+    def generator():
+        player = Player()
+        for chunk in sine(freq, seconds * RATE, PERIOD):
+            player.play(chunk)
+
+    plr_thread = threading.Thread(target=generator)
+    plr_thread.daemon = True
+    plr_thread.start()
+
+    rec = Recorder()
+    samples = rec.record(seconds * RATE)
+
+    Y = numpy.fft.fft(samples)
+    freqs = [abs(y) for y in Y[:int(RATE/2)]]
+    dominant = freqs.index(max(freqs)) / seconds
+    print("Dominant frequency is {}, expected {}".format(dominant, freq))
+
+    epsilon = 1.0
+    if abs(dominant - freq) < epsilon:
+        return 0
+    else:
+        return 1
+
+
 def main():
     actions = {
-        'playback': playback_test
+        'playback': playback_test,
+        'loopback': loopback_test,
     }
     parser = argparse.ArgumentParser(description='Sound testing using ALSA')
     parser.add_argument('action', metavar='ACTION', choices=actions.keys())
     parser.add_argument('--duration', type=int, default=5)
     args = parser.parse_args()
-    actions[args.action](args.duration)
+    return(actions[args.action](args.duration))
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
